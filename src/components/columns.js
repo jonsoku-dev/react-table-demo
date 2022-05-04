ffb701efbbefd52ed3b6c7cc2804e9ceafe84eb7
jonsoku2 <whdtjr2792@gmail.com>
2022-05-04 10:18:07 +0900

    step2

---
+++ src/components/columns.js
@@ -0,0 +1,21 @@
+export const COLUMNS = [
+    {
+        Header: 'Id',
+    },
+    {
+        Header: 'First Name'
+    },
+    {
+        Header: 'Last Name'
+    },
+    {
+        Header: 'Date of Birth'
+    },
+    {
+        Header: 'Country'
+    },
+    {
+        Header: 'Phone'
+    }
+
+]
