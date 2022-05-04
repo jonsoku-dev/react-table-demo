f4f460a19b2ab6686b6dc23d2647435a546e2edb
jonsoku2 <whdtjr2792@gmail.com>
2022-05-04 20:22:06 +0900

    Selecting rows

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
-import { BasicTable } from './components/BasicTable';
-import { FilteringTable } from './components/FilteringTable';
-import { PagenationTable } from './components/PagenationTable';
-import { SortingTable } from './components/SortingTable';
+import "./App.css";
+import { BasicTable } from "./components/BasicTable";
+import { FilteringTable } from "./components/FilteringTable";
+import { PagenationTable } from "./components/PagenationTable";
+import { RowSelection } from "./components/RowSelection";
+import { SortingTable } from "./components/SortingTable";
 
 function App() {
-  return (
-    // <BasicTable />
-    // <SortingTable />
-    // <FilteringTable />
-    <PagenationTable />
-  );
+    return (
+        // <BasicTable />
+        // <SortingTable />
+        // <FilteringTable />
+        // <PagenationTable />
+        <RowSelection />
+    );
 }
 
 export default App;
